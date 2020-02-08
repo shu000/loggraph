806fb9b29e2f0a85efd8a06b759b56835bb8d9cb
miyagon <hiim.shu.m@gmail.com>
2020-02-08 13:14:37 +0000

    update: setting webpack with typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,21 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/app.js',
+  entry: './src/app.ts',
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  // import 文で .ts ファイルを解決するため
+  resolve: {
+    extensions: ['.ts'],
   },
 };
