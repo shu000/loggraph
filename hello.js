6de12fba1e123a565f168d46996536b5e684cb27
miyagon <hiim.shu.m@gmail.com>
2020-02-08 08:15:05 +0000

    add: typescript

--- hello.js
+++
@@ -1,6 +0,0 @@
-const hello = () => {
-  const str = 'hello, world';
-  console.log(str);
-};
-
-hello();
---
+++ hello.ts
@@ -0,0 +1,6 @@
+const hello = (s: string): number => {
+  console.log(s);
+  return 0;
+};
+
+hello("hello, world");
