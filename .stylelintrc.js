11f6851384176501753d6f79a49bd284614b6ede
miyagon <hiim.shu.m@gmail.com>
2020-02-09 22:18:49 +0000

    add: stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    "stylelint-order"
+  ],
+  extends: [
+    "stylelint-config-recommended-scss",
+    "stylelint-config-standard",
+    "stylelint-prettier/recommended"
+  ],
+  ignoreFiles: [
+    "**/node_modules/**"
+  ],
+  rules: {
+    indentation: 2,
+    "string-quotes": "single",
+    "order/properties-alphabetical-order": true
+  }
+};
