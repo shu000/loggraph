35ef9acde2c0f4da50fbb0d492403ae07cdfdc8f
miyagon <hiim.shu.m@gmail.com>
2020-02-09 03:49:13 +0000

    fix: eslint with tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 import './App.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="App" >
+    <div className="App">
       <h1>hello</h1>
     </div>
   );
