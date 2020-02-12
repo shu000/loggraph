42b384c43e95aa05732478dac95bce7b05b31118
miyagon <hiim.shu.m@gmail.com>
2020-02-12 07:54:43 +0000

    fix: set port by process.env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 80;
+const port = process.env.APP_PORT;
 const { Rules } = require('./server/rules');
 
 app.get('/api/customers', async (req, res) => {
